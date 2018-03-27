7034b932f9da77d21155874de8f4a1f67ed7e729
Lunaskaa <bababa1150@gmail.com>
2018-03-27 20:52:52 +0700

    Update mrtg-mem.sh

--- mrtg-mem.sh
+++ mrtg-mem.sh
@@ -14,28 +14,7 @@ if [ "$MYIP" = "" ]; then
 fi
 #MYIP=$(wget -qO- ipv4.icanhazip.com)
 
-#vps="zvur";
-vps="aneka";
 
-
-	source="https://raw.githubusercontent.com/bababa358/test"
-
-
-# go to root
-cd
-
-# check registered ip
-wget -q -O IP $source/debian7/IP.txt
-if ! grep -w -q $MYIP IP; then
-	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
-	if [[ $vps = "zvur" ]]; then
-		echo "Hubungi: Yusuf ardiansyah atau Elang overdosis"
-	else
-		echo "Hubungi: Yusuf ardiansyah atau Elang overdosis"
-	fi
-	rm -f /root/IP
-	exit
-fi
 
 
 FREE=`free -m | grep "buffers/cache" | awk '{print $3}'`
